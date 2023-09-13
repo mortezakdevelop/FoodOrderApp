e12c78d26c43c7cec788cd1d2efd8c60a5a8e3ab
Morteza <morteza.kalaate2105@gmail.com>
2023-09-13 09:29:19 -0700

    add all libraries

---
+++ app/src/main/java/com/morteza/foodorderapplication/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.morteza.foodorderapplication.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.morteza.foodorderapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
