7ccdbeb6cf5b5fbedf50750f3a2b48c5ca195d7b
mortezakdevelop <m.vortex3537007@gmail.com>
2023-09-17 15:28:13 -0700

    splash screen and view bindig

--- app/src/main/java/com/morteza/foodorderapplication/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/morteza/foodorderapplication/ui/fragment/SplashFragment.kt
@@ -5,20 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import coil.load
 import com.morteza.foodorderapplication.R
+import com.morteza.foodorderapplication.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    private lateinit var binding: FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.apply {
+            bgImg.load(R.drawable.bg_splash)
+        }
     }
 }
