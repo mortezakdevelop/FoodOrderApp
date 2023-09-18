d5b436cdd518716dd3df9afd1f319cbd0962d79b
Morteza <morteza.kalaate2105@gmail.com>
2023-09-18 08:21:49 -0700

    strings and corutine scope

--- app/src/main/java/com/morteza/foodorderapplication/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/morteza/foodorderapplication/ui/fragment/SplashFragment.kt
@@ -1,11 +1,14 @@
 package com.morteza.foodorderapplication.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import coil.load
+import com.morteza.foodorderapplication.BuildConfig
 import com.morteza.foodorderapplication.R
 import com.morteza.foodorderapplication.databinding.FragmentSplashBinding
 
@@ -20,11 +23,17 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
             bgImg.load(R.drawable.bg_splash)
+            versionTxt.text = "${getString(R.string.version)} : ${BuildConfig.VERSION_NAME}"
+
+            lifecycleScope.launchWhenCreated {
+
+            }
         }
     }
 }
