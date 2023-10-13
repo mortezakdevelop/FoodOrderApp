f5ee051d6587f799e94dd31d92e5a6d296c9a26d
mortezakdevelop <m.vortex3537007@gmail.com>
2023-10-13 07:56:30 -0700

    bottom navigation

--- app/src/main/java/com/morteza/foodorderapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/morteza/foodorderapplication/ui/activity/MainActivity.kt
@@ -2,18 +2,39 @@ package com.morteza.foodorderapplication.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.morteza.foodorderapplication.R
 import com.morteza.foodorderapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.mainBottomAppbar.background = null
         setContentView(binding.root)
+
+        binding.mainBottomNav.background = null
+
+        navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.mainBottomNav.setupWithNavController(navHost.navController)
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onNavigateUp()
     }
 }
